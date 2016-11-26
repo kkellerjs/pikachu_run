900d827f6734ab38fce3a94891b67bbb666c5534
kkellerjs <kkellerb@gmu.edu>
2016-11-26 17:31:39 -0500

    Update background.js
    No longer using on/off listener via icon click.

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 
 
 
-//when click icon...
-chrome.browserAction.onClicked.addListener(function(tab) {
+//when click icon...have to change this anyway.
+//chrome.browserAction.onClicked.addListener(function(tab) {
 
-
-
+/*
   chrome.storage.local.get("onOff", function (data) {
   
-   console.log(data.onOff);
+  console.log(data);
  
    if (data.onOff == "on") {
-    chrome.storage.local.set({"onOff": "off"}, function(){
+    chrome.storage.local.set({diamond: "off"}, function(){
      chrome.browserAction.setIcon({path:{"16": "16grey.png", "48": "48grey.png","128": "128grey.png"}});
 	 });
 	 
@@ -25,4 +24,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   
   
 
-});
+//});
+
+*/
